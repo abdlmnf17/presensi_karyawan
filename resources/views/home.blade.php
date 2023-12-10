56dc9ddcac9f92b1cc1c725c721997f545545021
abdlmnf17 <11210195@bsi.ac.id>
2023-12-10 20:33:07 +0700

    add: frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,13 @@
                         </div>
                     @endif
 
-                    {{ __('Selamat Datang ')}}, {{ Auth::user()->name }}.
+                    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                        <strong>  {{ __('Selamat Datang ')}}, {{ Auth::user()->name }}.</strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,32 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+     <!-- Bootstrap Icons -->
+
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.17.0/font/bootstrap-icons.css" rel="stylesheet">
+
+
     <!-- Scripts -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <style>
+  #sidebar {
+            min-width: 225px;
+            max-width: 225px;
+            min-height: 100vh;
+        }
+
+        .nav-link i {
+            margin-right: 5px;
+        }
+
+        .nav-link {
+            display: flex;
+            align-items: center;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
@@ -33,45 +57,95 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
 
+
                     </ul>
+            {{-- Centre Navbar --}}
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="karyawanDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="material-icons">badge</i> Karyawan
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="karyawanDropdown">
+                        <a class="dropdown-item" href="/karyawan">Data Karyawan</a>
+                        <a class="dropdown-item" href="#">Lembur Karyawan</a>
+                        <a class="dropdown-item" href="/presensi">Kehadiran</a>
+                        <a class="dropdown-item" href="/presensi">Lemburan</a>
+                    </div>
+                </li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="presensiDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="material-icons">event_available</i> Presensi
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="presensiDropdown">
+                        <a class="dropdown-item" href="/presensi">Submenu 1</a>
+                        <a class="dropdown-item" href="#">Submenu 2</a>
+                        <a class="dropdown-item" href="#">Submenu 3</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="cutiDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="material-icons">calendar_view_month</i> Cuti
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="cutiDropdown">
+                        <a class="dropdown-item" href="/cuti">Cuti Karyawan</a>
+                        <a class="dropdown-item" href="#">Cuti HRD</a>
+                        {{-- <a class="dropdown-item" href="#">Submenu 3</a> --}}
+                    </div>
+                </li>
 
 
-                        @endguest
-                    </ul>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto">
+               
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">
+                                <i class="bi bi-box-arrow-in-right"></i> {{ __('Login') }}
+                            </a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">
+                                <i class="bi bi-person-plus"></i> {{ __('Register') }}
+                            </a>
+                        </li>
+                    @endif
+                    @else
+
+                    <li class="nav-item dropdown">
+
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <i class="material-icons">
+                                account_circle
+                                </i> {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+
+
+                @endguest
+            </ul>
+
+
+
                 </div>
             </div>
         </nav>
@@ -80,5 +154,12 @@
             @yield('content')
         </main>
     </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>
+$('.alert').alert()
+    </script>
+
 </body>
 </html>
