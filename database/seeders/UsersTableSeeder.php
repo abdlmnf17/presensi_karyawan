f2a531b7c5e55a226d8b51db16d70a1f175af9ed
abdlmnf17 <11210195@bsi.ac.id>
2023-12-20 18:51:02 +0700

    add: seeders

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash; // Tambahkan use statement untuk Hash
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Tambahkan user dengan peran Admin
+        User::create([
+            'name' => 'Nama Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'), // Gunakan Hash dari Illuminate\Support\Facades
+            'role' => 'admin', // Sesuaikan dengan kolom yang menandakan peran pada model User
+        ]);
+
+        // Tambahkan user lain jika diperlukan
+        // User::create([
+        //     'name' => 'Nama User',
+        //     'email' => 'user@example.com',
+        //     'password' => Hash::make('password'),
+        //     'role' => 'user',
+        // ]);
+
+        // Tambahkan kode seeder lainnya jika diperlukan
+    }
+}
